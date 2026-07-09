da168c169a0410c6e29297a6a03bd733848eeab3
Timmy <baoly069@gmail.com>
2026-07-09 10:28:12 +0700

    add bootstrap lib

---
+++ data/products/products.js
@@ -0,0 +1,17 @@
+const products = [
+    {id:1, name:"IPhone 11 Promax", quality:22000000,image:`../public/imgs/p1.png`},
+    {id:2, name:"IPhone 12 Promax", quality:25000000},
+    {id:3, name:"IPhone 13 Promax", quality:27000000},
+    {id:4, name:"IPhone 14 Promax", quality:29000000},
+    {id:5, name:"IPhone 15 Promax", quality:32000000},
+    {id:6, name:"IPhone 16 Promax", quality:35000000},
+    {id:7, name:"Samsung Galaxy S21", quality:18000000},
+    {id:8, name:"Samsung Galaxy S22", quality:20000000},
+    {id:9, name:"Samsung Galaxy S23", quality:23000000},
+    {id:10, name:"Samsung Galaxy S24", quality:26000000},
+    {id:11, name:"Xiaomi 13 Pro", quality:15000000},
+    {id:12, name:"Xiaomi 14 Pro", quality:17000000},
+    {id:13, name:"Oppo Find X6", quality:16000000},
+    {id:14, name:"Oppo Find X7", quality:19000000},
+    {id:15, name:"Vivo X100 Pro", quality:21000000}
+]
